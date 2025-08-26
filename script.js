cf82089bdfbd4523c469bb1069c7d616126608bf
Leolfc <leonardofeliciano81@gmail.com>
2025-08-26 11:45:35 -0300

    adicionando mais moedas

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const nomeMoeda = document.querySelector("#nomeMoeda");
 function moeda() {
   const dolarToday = 5.42;
   const euroToday = 6.3;
+  const libraToday = 7.32;
+  const bitcoinToday = 597943;
 
   if (selectFLag.value === "dolar") {
     valorConvertido.innerHTML = new Intl.NumberFormat("en-US", {
@@ -19,9 +21,19 @@ function moeda() {
   }
   if (selectFLag.value === "euro") {
     valorConvertido.innerHTML = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
       currency: "EUR",
     }).format(input.value / euroToday);
     nomeMoeda.innerHTML = "Euro";
+  }
+  if (selectFLag.value === "libra") {
+    valorConvertido.innerHTML = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "GBP",
+    }).format(input.value / libraToday);
+  }
+  if(selectFLag.value === "bitcoin"){
+    valorConvertido.innerHTML = new Intl.NumberFormat('pt-BR',{style: "currency", currency:'BTC'}).format(input.value/ bitcoinToday)
   }
 
   currencyBr.innerHTML = new Intl.NumberFormat("pt-BR", {
@@ -45,6 +57,14 @@ selectFLag.addEventListener("change", () => {
   if (selectFLag.value === "euro") {
     img.src = " assets/euro.png";
     nomeMoeda.innerHTML = "Euro";
+  }
+  if (selectFLag.value === "libra") {
+    img.src = "assets/libra.png";
+    nomeMoeda.innerHTML = "Libra"
+  }
+  if (selectFLag.value === "bitcoin") {
+    img.src = "assets/bitcoin.png";
+    nomeMoeda.innerHTML = "Bitcoin"
   }
   moeda();
 });
